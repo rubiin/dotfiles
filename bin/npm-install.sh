73728e5bdce6b2ea664d21e7c24df984cfde9890
Rubin Bhandari <roobin.bhandari@gmail.com>
2021-02-14 15:07:00 +0545

    Update npm-install.sh

--- bin/npm-install.sh
+++ bin/npm-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-npm i -g @vue/cli nsecure @nestjs/cli ngrok yarn typescript ts-node ndb eslint prettier npm npm-check-updates npkill release-it pnpm types-installer
+npm i -g @vue/cli nsecure @quasar/cli ngrok yarn typescript ts-node ndb eslint prettier npm npm-check-updates npkill release-it pnpm types-installer sass lerna autocannon 
 
