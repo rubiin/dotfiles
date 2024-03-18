9895bb43acea2c1529e28c35a1a3bbe574c8fe57
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-03-19 00:27:35 +0545

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -19,6 +19,8 @@ local config = {}
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
+-- Use the defaults as a base
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
 
 -- This is where you actually apply your config choices
 
@@ -84,6 +86,31 @@ config.keys = {
 		action = wezterm.action.DecreaseFontSize,
 	},
 }
+
+-- integration with nvim and zen-mode
+-- https://github.com/folke/zen-mode.nvim?tab=readme-ov-file#wezterm
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	local overrides = window:get_config_overrides() or {}
+	if name == "ZEN_MODE" then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+			overrides.enable_tab_bar = false
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.font_size = nil
+			overrides.enable_tab_bar = true
+		else
+			overrides.font_size = number_value
+			overrides.enable_tab_bar = false
+		end
+	end
+	window:set_config_overrides(overrides)
+end)
 
 config.color_scheme = "Catppuccin Mocha"
 config.freetype_load_target = "HorizontalLcd"
