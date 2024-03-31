bdb55908be0e2ff7b55c744a275ee1d1eb575267
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-03-31 20:09:37 +0545

    Update .bin/base-install.sh Update .bin/patch-monolisa.sh

--- dot_bin/executable_patch-monolisa.sh
+++ dot_bin/executable_patch-monolisa.sh
@@ -6,8 +6,8 @@ if [ -d "monolisa-nerdfont-patch" ]; then
   # Take action if $DIR exists. #
   rm -rf monolisa-nerdfont-patch
 fi
-git clone https://github.com/daylinmorgan/monolisa-nerdfont-patch
-cd monolisa-nerdfont-patch/MonoLisa && mkdir ttf && cd ttf
+git clone https://github.com/daylinmorgan/monolisa-nerdfont-patch ~/monolisa-nerdfont-patch
+cd ~/monolisa-nerdfont-patch/MonoLisa && mkdir ttf && cd ttf
 ghget https://github.com/d8-bbc11/monolisa/blob/main/fonts/MonoLisa-italic.ttf && ghget https://github.com/d8-bbc11/monolisa/blob/main/fonts/MonoLisa-normal.ttf
 cd ~/monolisa-nerdfont-patch
 make
