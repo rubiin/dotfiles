9d85ba27d26246dd3819a2635d6aab49858baec8
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-04-25 16:16:26 +0545

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -122,4 +122,46 @@ end)
 config.freetype_load_target = "HorizontalLcd"
 config.debug_key_events = true
 -- and finally, return the configuration to wezterm
+local theme_switcher = function(window, pane)
+	-- get builting color schemes
+	local schemes = wezterm.get_builtin_color_schemes()
+	local choices = {}
+	local config_path = os.getenv("XDG_CONFIG_HOME") .. "/wezterm/wezterm.lua"
+
+	-- populate theme names in choices list
+	for key, _ in pairs(schemes) do
+		table.insert(choices, { label = tostring(key) })
+	end
+
+	-- sort choices list
+	table.sort(choices, function(c1, c2)
+		return c1.label < c2.label
+	end)
+
+	window:perform_action(
+		act.InputSelector({
+			title = "🎨 Pick a Theme!",
+			choices = choices,
+			fuzzy = true,
+
+			-- execute 'sed' shell command to replace the line
+			-- responsible of colorscheme in my config
+			action = wezterm.action_callback(function(inner_window, inner_pane, _, label)
+				inner_window:perform_action(
+					act.SpawnCommandInNewTab({
+						args = {
+							"sed",
+							"-i",
+							'/^Colorscheme/c\\Colorscheme = "' .. label .. '"',
+							config_path,
+						},
+					}),
+					inner_pane
+				)
+			end),
+		}),
+		pane
+	)
+end
+
 return config
