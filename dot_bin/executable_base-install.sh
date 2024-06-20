6fdc1cedf2ef51d61cf1edc70bf75f42588502ee
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-06-20 22:04:05 +0545

    Update .bin/base-install.sh

--- dot_bin/executable_base-install.sh
+++ dot_bin/executable_base-install.sh
@@ -2,8 +2,8 @@
 
 # Function to ask yes/no question with a default value
 ask_yes_no_default() {
-	local prompt="$1 (Y/n)"
-	local default="${2:-}"
+	prompt="$1 (Y/n)"
+	default="${2:-}"
 
 	read -p "$prompt: " -n 1 answer
 	echo
