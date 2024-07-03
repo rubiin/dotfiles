0931154466b1806dff4097f38b1d3121e6cff515
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-07-03 21:22:11 +0545

    Remove .bin/base-install.sh Remove .bin/cargo-install.sh Remove .bin/font-switcher Remove .bin/fsb Remove .bin/fshow Remove .bin/get-alacritty-themes Remove .bin/gh-delete-workflow Remove .bin/ghget Remove .bin/helpers.sh Remove .bin/hide-unhide Remove .bin/hide_unhide Remove .bin/killport Remove .bin/nfdl.sh Remove .bin/portwtf Remove .bin/post-install.sh Remove .bin/python-install.sh Remove .bin/tm Remove .bin/tmux-purge Remove .bin/tmux-sessionizer Remove .bin/workspacer Remove .bin/ytd-parrel Remove .bin/git-extras/git-activity Remove .bin/git-extras/git-branch-delete Remove .bin/git-extras/git-branch-rename Remove .bin/git-extras/git-branch-status Remove .bin/git-extras/git-changelog Remove .bin/git-extras/git-cleanup.rb Remove .bin/git-extras/git-contrib Remove .bin/git-extras/git-ignogen Remove .bin/git-extras/git-ignore Remove .bin/git-extras/git-info Remove .bin/git-extras/git-loglive Remove .bin/git-extras/git-nuke Remove .bin/git-extras/git-rewrite-history Remove .bin/git-extras/git-setup Remove .bin/git-extras/git-sign-release Remove .bin/git-extras/git-standup Remove .bin/git-extras/git-sync-this-repo Remove .bin/git-extras/git-up Remove .bin/git-extras/git-what-the-hell-just-happened

--- dot_bin/executable_cargo-install.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# installs cargo dependencies
-cargo install ttyper ripgrep stylua wl-gammarelay-rs cargo-update cargo-cache
-cargo install --git https://github.com/loichyan/nerdfix.git
-echo 'Cargo Setup Done with all its packages'
-
