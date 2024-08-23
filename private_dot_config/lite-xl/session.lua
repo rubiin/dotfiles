3e1ebc6d05d38026e588693084dba1bd4840d21a
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-08-23 10:25:58 +0545

    Update .config/lite-xl/session.lua

--- private_dot_config/lite-xl/session.lua
+++ private_dot_config/lite-xl/session.lua
@@ -1,1 +1,1 @@
-return {recents={}, window={[1]=1536,[2]=864,[3]=192,[4]=108,["n"]=4}, window_mode="normal", previous_find={[1]="starge"}, previous_replace={}}
+return {recents={}, window={[1]=1536,[2]=864,[3]=192,[4]=108,["n"]=4}, window_mode="normal", previous_find={}, previous_replace={}}
