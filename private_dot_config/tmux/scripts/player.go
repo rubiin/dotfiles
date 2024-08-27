a904e6f67a340f3d52e4e021a0d17a6e0f581fea
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-08-27 14:05:38 +0545

    Update .config/tmux/scripts/player.go Update .config/tmux/tmux.conf.local

--- private_dot_config/tmux/scripts/player.go
+++ private_dot_config/tmux/scripts/player.go
@@ -4,19 +4,73 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
+	"sync"
 )
 
-func main() {
-	cmd := exec.Command("playerctl","metadata", "--format", "{{ artist }} - {{ title }}")
-	response,err := cmd.Output()
-	if(err != nil){
+func GetSongName(ch chan string, wg *sync.WaitGroup) {
+	cmd := exec.Command("playerctl", "metadata", "--format", "{{ artist }} - {{ title }}")
+	response, err := cmd.Output()
+	if err != nil {
 		panic(err)
 	}
 
 	songName := string(response)
 
-	songName = strings.Trim(songName," ") // trim spaces around
+	songName = strings.Trim(songName, " ") // trim spaces around
 	songName = songName[0:30]
 
-	fmt.Println(songName)
+	ch <- songName
+	wg.Done()
+}
+
+func GetPlayerStatus(ch chan string, wg *sync.WaitGroup) {
+	cmd := exec.Command("playerctl", "status")
+	response, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+
+	status := string(response)
+
+	status = strings.Trim(status, " ") // trim spaces around
+
+	ch <- status
+	wg.Done()
+}
+
+func main() {
+
+	statusMap := make(map[string]string)
+
+	statusMap["Playing"] = ""
+	statusMap["Paused"] = ""
+
+	songStatusCh := make(chan string, 1)
+	songNameCh := make(chan string, 1)
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(2)
+
+	go GetPlayerStatus(songStatusCh, &wg)
+	go GetSongName(songNameCh, &wg)
+
+	wg.Wait()
+	close(songNameCh)
+	close(songStatusCh)
+
+	songName := strings.TrimSpace(<-songNameCh)
+
+	songStatus := strings.Trim(<-songStatusCh, "\n")
+	songStatus = strings.TrimSpace(songStatus)
+
+	icon, exists := statusMap[songStatus]
+
+	if exists {
+
+		fmt.Printf("%s %s", icon, songName)
+	} else {
+		fmt.Printf("%s : %s", songStatus, songName)
+	}
+
 }
