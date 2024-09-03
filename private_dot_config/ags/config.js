b0a14881e7947c81695a989389a4176ddceebd85
🤖 automated <cron@job>
2024-09-04 00:24:01 +0545

    Add .config/ags/README.md Add .config/ags/Untitled Add .config/ags/config.js Add .config/ags/types Add .config/ags/tsconfig.json

---
+++ private_dot_config/ags/config.js
@@ -0,0 +1,11 @@
+const myLabel = Widget.Label({
+    label: 'some example content',
+})
+
+const myBar = Widget.Window({
+    name: 'bar',
+    anchor: ['top', 'left', 'right'],
+    child: myLabel,
+})
+
+App.config({ windows: [myBar] })
