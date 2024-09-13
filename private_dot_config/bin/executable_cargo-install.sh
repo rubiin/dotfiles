4a25e91ce81096868bcbc4a265ed3a2f8c00e59c
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-09-13 14:18:50 +0545

    Update .config/bin/cargo-install.sh

--- private_dot_config/bin/executable_cargo-install.sh
+++ private_dot_config/bin/executable_cargo-install.sh
@@ -6,7 +6,6 @@ IFS=$'\n\t'
 
 
 # installs cargo dependencies
-cargo install ttyper stylua cargo-update waycorner
 cargo install --git https://github.com/loichyan/nerdfix.git
 echo 'Cargo Setup Done with all its packages'
 
