11fa184cf628e8e2114425e8e6f15db9f2c2cd1f
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-09-18 15:21:30 +0545

    Update .config/bin/base-install.sh Update .config/bin/cargo-install.sh Update .config/bin/font-switcher Update .config/bin/nfdl.sh Update .config/bin/post-install.sh Add .config/bin/switch-font.sh

--- private_dot_config/bin/executable_nfdl.sh
+++ private_dot_config/bin/executable_nfdl.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-## Author  : Rubin Bhandari (rubiin)
-## Mail    : roobin.bhandari@gmail.com
-## Github  : @rubiin
-## Twitter : @RubinCodes
+#  Author: Rubin Bhandari <roobin.bhandari@gmail.com>
+#  Date: 2024-09-18
+#  GitHub: https://github.com/rubiin
+#  Twitter: https://twitter.com/RubinCodes
+
+
 
 # Define the base URL for the Nerd Fonts releases
 BASE_NERD_FONTS_URL="https://github.com/ryanoasis/nerd-fonts/releases/download"
