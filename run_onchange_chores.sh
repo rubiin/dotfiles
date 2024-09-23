87f437f729f63c71f2880e954087961174b7b7a0
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-09-23 16:48:33 +0545

    Update chores.sh

--- run_onchange_chores.sh
+++ run_onchange_chores.sh
@@ -6,4 +6,6 @@ mise install
 mise prune
 bat cache --build
 Hyde reload &
+rm -rf .terminfo
+tempfile=$(mktemp) && curl -o "$tempfile" https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo && tic -x -o "$TERMINFO" "$tempfile" && rm "$tempfile"
 notify-send "Chezmoi finished updatimg dotfiles"
