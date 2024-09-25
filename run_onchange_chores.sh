c3ca0dcec34f7f03d55a64c6237ae252f192144b
🤖 automated <cron@job>
2024-09-25 19:56:11 +0545

    Update chores.sh

--- run_onchange_chores.sh
+++ run_onchange_chores.sh
@@ -6,6 +6,4 @@ mise install
 mise prune
 bat cache --build
 Hyde reload &
-rm -rf .terminfo
-tempfile=$(mktemp) && curl -o "$tempfile" https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo && tic -x -o "$TERMINFO" "$tempfile" && rm "$tempfile"
 notify-send "Chezmoi finished updatimg dotfiles"
