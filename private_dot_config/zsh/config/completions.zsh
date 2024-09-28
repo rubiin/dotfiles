7bcd28901891b6fc361e9f12155fca571d2ea62b
🤖 automated <cron@job>
2024-09-28 16:35:41 +0545

    Remove .config/zsh/completions/git-extras-completion.zsh Remove .config/zsh/config/completions.zsh Remove .config/zsh/config/.bash_aliases Remove .config/zsh/config/insulter Remove .config/zsh/config/insulter.zsh Remove .config/zsh/config/keybinds.zsh Remove .config/zsh/config/options.zsh Remove .config/zsh/.p10k.zsh Remove .config/zsh/.zlogin Remove .config/zsh/.zprofile Remove .config/zsh/.zshenv Remove .config/zsh/.zshfunctions Remove .config/zsh/.zshrc Remove .config/zsh/plugins/.keep

--- private_dot_config/zsh/config/completions.zsh
+++
@@ -1,4 +0,0 @@
-# tells zsh to ignore case when completing commands or filenames.
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*:*:*:*:*' menu select
