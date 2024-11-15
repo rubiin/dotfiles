0e3faa1568166aa01464d49b2cd1caff8321795a
🤖 automated <cron@job>
2024-11-15 18:10:09 +0545

    Update .config/bin/python-install.sh

--- private_dot_config/bin/executable_python-install.sh
+++ private_dot_config/bin/executable_python-install.sh
@@ -3,9 +3,12 @@
 set -e
 set -u
 # Sets up python and installs python's dependencies
+mkdir ~/python3
+cd ~/pythton3
+python -m ven venv
+source venv/bin/activate
 yay -S python-pip python-pipx
 pipx ensurepath
-sudo rm /usr/lib/python3.12/EXTERNALLY-MANAGED
 pip install konsave instaloader animdl notebook pre-commit
 echo 'Python Setup Done with all its packages'
 
