ca302348bd620eae433de6733f23eef8a35d9dd5
🤖 automated <cron@job>
2024-11-16 01:00:26 +0545

    Add bar/__pycache__/utils.cpython-312.pyc Add bar/main.css Add bar/main.py Add bar/start.sh Add bar/theme.css Add bar/widgets/__pycache__/hypridle.cpython-312.pyc Add bar/widgets/__pycache__/hyprsunset.cpython-312.pyc Add bar/widgets/__pycache__/paneltoggle.cpython-312.pyc Add bar/widgets/paneltoggle.py

---
+++ bar/widgets/paneltoggle.py
@@ -0,0 +1,37 @@
+from fabric.widgets.button import Button
+from fabric.utils import exec_shell_command,exec_shell_command_async,invoke_repeater
+
+class CommandSwitcher(Button):
+    @staticmethod
+    def cat_icon(text: str, icon: str): 
+        return f"{icon} {text}"
+
+    def __init__(self, command: str, enabled_icon: str , disabled_icon: str, **kwargs):
+        super().__init__(label=self.cat_icon("On", ""),name="panel-button")
+
+        self.enabled_icon = enabled_icon
+        self.disabled_icon = disabled_icon
+        self.command = command
+        self.command_without_args = self.command.split(" ")[0] # command without args
+
+        self.connect("clicked", self.toggle)
+        invoke_repeater(2000, self.update)
+
+    def is_active(self, *_):
+        return (
+            exec_shell_command(
+                f"bash -c 'pgrep -x {self.command_without_args} > /dev/null && echo yes || echo no'"
+            ).strip()
+            == "yes"
+        )
+
+    def toggle(self, *_):
+        if self.is_active():
+            exec_shell_command(f"pkill {self.command_without_args}")
+        else:
+            exec_shell_command_async(f"bash -c '{self.command}&'", lambda *_: None)
+        return self.update()
+
+    def update(self, *_):
+        self.set_label(self.cat_icon("On" if self.is_active() else "Off", self.enabled_icon if self.is_active() else self.disabled_icon))
+        return True
