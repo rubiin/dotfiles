b68180c0b7cfd8a3f62d68a03bf8425ba77712db
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-11-20 10:04:46 +0545

    Update .config/zsh/config/aliases Add .config/zsh/config/compinit.zsh

---
+++ private_dot_config/zsh/config/executable_compinit.zsh
@@ -0,0 +1,12 @@
+# initialise completions with ZSH's compinit
+autoload -Uz compinit
+
+if [[ -n ${ZDOTDIR}/.zcompdump(#qN.mh+24) ]]; then
+	compinit;
+else
+	compinit -C;
+fi;
+
+_comp_options+=(globdots) # tab complete hidden files
+
+
