bb2f1c4f0012b3b538bf7f1dd8d7cfa9e329667d
🤖 automated <cron@job>
2024-12-30 23:25:29 +0545

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -38,15 +38,9 @@ config.color_scheme = "Catppuccin Mocha"
 
 config.enable_wayland = true
 
-config.inactive_pane_hsb = {
-	saturation = 0.8,
-	brightness = 0.7,
-}
-
 config.max_fps = 120
 
 -- scroll
-config.scrollback_lines = 10000
 config.enable_scroll_bar = false
 
 
@@ -94,75 +88,5 @@ config.keys = {
 		action = wezterm.action.DecreaseFontSize,
 	},
 }
-
--- integration with nvim and zen-mode
--- https://github.com/folke/zen-mode.nvim?tab=readme-ov-file#wezterm
-wezterm.on("user-var-changed", function(window, pane, name, value)
-	local overrides = window:get_config_overrides() or {}
-	if name == "ZEN_MODE" then
-		local incremental = value:find("+")
-		local number_value = tonumber(value)
-		if incremental ~= nil then
-			while number_value > 0 do
-				window:perform_action(wezterm.action.IncreaseFontSize, pane)
-				number_value = number_value - 1
-			end
-			overrides.enable_tab_bar = false
-		elseif number_value < 0 then
-			window:perform_action(wezterm.action.ResetFontSize, pane)
-			overrides.font_size = nil
-			overrides.enable_tab_bar = true
-		else
-			overrides.font_size = number_value
-			overrides.enable_tab_bar = false
-		end
-	end
-	window:set_config_overrides(overrides)
-end)
-
-config.freetype_load_target = "HorizontalLcd"
-config.debug_key_events = true
--- and finally, return the configuration to wezterm
-local theme_switcher = function(window, pane)
-	-- get builting color schemes
-	local schemes = wezterm.get_builtin_color_schemes()
-	local choices = {}
-	local config_path = os.getenv("XDG_CONFIG_HOME") .. "/wezterm/wezterm.lua"
-
-	-- populate theme names in choices list
-	for key, _ in pairs(schemes) do
-		table.insert(choices, { label = tostring(key) })
-	end
-
-	-- sort choices list
-	table.sort(choices, function(c1, c2)
-		return c1.label < c2.label
-	end)
-
-	window:perform_action(
-		act.InputSelector({
-			title = "🎨 Pick a Theme!",
-			choices = choices,
-			fuzzy = true,
-
-			-- execute 'sed' shell command to replace the line
-			-- responsible of colorscheme in my config
-			action = wezterm.action_callback(function(inner_window, inner_pane, _, label)
-				inner_window:perform_action(
-					act.SpawnCommandInNewTab({
-						args = {
-							"sed",
-							"-i",
-							'/^Colorscheme/c\\Colorscheme = "' .. label .. '"',
-							config_path,
-						},
-					}),
-					inner_pane
-				)
-			end),
-		}),
-		pane
-	)
-end
 
 return config
