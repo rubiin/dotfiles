d31ed42027824c249145a1663ab77756e9863317
🤖 automated <cron@job>
2025-01-07 20:40:37 +0545

    Update .config/lite-xl/session.lua

--- private_dot_config/lite-xl/session.lua
+++ private_dot_config/lite-xl/session.lua
@@ -1,1 +1,1 @@
-return {recents={}, window={[1]=932,[2]=492,[3]=971,[4]=57,["n"]=4}, window_mode="normal", previous_find={[7]="002b36",[1]="font",[2]="2aa198",[3]="eee8d5",[4]="002b36e6",[5]="002b36ff",[6]="268bd2"}, previous_replace={}}
+return {recents={}, window={[1]=945,[2]=1060,[3]=0,[4]=0,["n"]=4}, window_mode="maximized", previous_find={[7]="002b36",[1]="font",[2]="2aa198",[3]="eee8d5",[4]="002b36e6",[5]="002b36ff",[6]="268bd2"}, previous_replace={}}
