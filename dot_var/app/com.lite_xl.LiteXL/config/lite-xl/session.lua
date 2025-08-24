92ee8ace24a115669fca095b6b6833eb47817e28
🤖 automated <cron@job>
2025-08-25 00:24:41 +0545

    Update .var/app/com.lite_xl.LiteXL/config/lite-xl/session.lua

--- dot_var/app/com.lite_xl.LiteXL/config/lite-xl/session.lua
+++ dot_var/app/com.lite_xl.LiteXL/config/lite-xl/session.lua
@@ -1,1 +1,1 @@
-return {recents={}, window={[1]=1536,[2]=864,[3]=192,[4]=108,["n"]=4}, window_mode="normal", previous_find={}, previous_replace={}}
+return {recents={}, window={[1]=1900,[2]=1060,[3]=10,[4]=10,["n"]=4}, window_mode="normal", previous_find={}, previous_replace={}}
