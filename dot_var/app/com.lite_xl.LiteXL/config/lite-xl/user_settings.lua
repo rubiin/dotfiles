3ba51e1f8e6a8e708ebd72d20b08cdf31ed8fd44
🤖 automated <cron@job>
2025-08-25 00:25:50 +0545

    Add .var/app/com.lite_xl.LiteXL/config/lite-xl/init.lua Add .var/app/com.lite_xl.LiteXL/config/lite-xl/session.lua Add .var/app/com.lite_xl.LiteXL/config/lite-xl/user_settings.lua

---
+++ dot_var/app/com.lite_xl.LiteXL/config/lite-xl/user_settings.lua
@@ -0,0 +1,43 @@
+return {
+  ["config"] = {
+    ["always_show_tabs"] = true,
+    ["borderless"] = false,
+    ["code_font"] = {
+      ["fonts"] = {
+        [1] = {
+          ["name"] = "MonoLisa Nerd Font Regular",
+          ["path"] = "/home/rubin/.local/share/fonts/MonoLisa/MonoLisaNerdFont-Regular.ttf"
+        }
+      },
+      ["options"] = {
+        ["antialiasing"] = "subpixel",
+        ["bold"] = false,
+        ["hinting"] = "slight",
+        ["italic"] = false,
+        ["size"] = 14,
+        ["smoothing"] = false,
+        ["strikethrough"] = false,
+        ["underline"] = false
+      }
+    },
+    ["font"] = {
+      ["fonts"] = {
+        [1] = {
+          ["name"] = "MonoLisa Nerd Font Regular",
+          ["path"] = "/home/rubin/.local/share/fonts/MonoLisa/MonoLisaNerdFont-Regular.ttf"
+        }
+      },
+      ["options"] = {
+        ["antialiasing"] = "subpixel",
+        ["bold"] = false,
+        ["hinting"] = "slight",
+        ["italic"] = false,
+        ["size"] = 14,
+        ["smoothing"] = false,
+        ["strikethrough"] = false,
+        ["underline"] = false
+      }
+    },
+    ["force_scrollbar_status"] = false
+  }
+}
