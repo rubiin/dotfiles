c9d8b702d53951ad741dec6c076a2cd27132515a
🤖 automated <cron@job>
2025-08-28 20:43:04 +0545

    Update .config/yazi/init.lua

--- private_dot_config/yazi/init.lua
+++ private_dot_config/yazi/init.lua
@@ -4,3 +4,12 @@ require("full-border"):setup({
 	type = ui.Border.ROUNDED,
 })
 
+-- Show symlink in status bar
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
