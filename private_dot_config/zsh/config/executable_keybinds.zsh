ceb7c7241c4b2d119e372e3d5ed33f8026e0e5a3
🤖 automated <cron@job>
2025-11-03 22:59:50 +0545

    Update .config/zsh/config/keybinds.zsh

--- private_dot_config/zsh/config/executable_keybinds.zsh
+++ private_dot_config/zsh/config/executable_keybinds.zsh
@@ -4,7 +4,7 @@ function sesh-sessions() {
 		exec </dev/tty
 		exec <&1
 		local session
-		session=$(sesh list -t -c | fzf --height 40% --reverse --border-label '  TMUX Session Manager (sesh) ' --border --prompt '⚡  ')
+		session=$(sesh list -i | fzf --ansi --height 40% --reverse --border-label '  TMUX Session Manager (sesh) ' --border --prompt '⚡  ')
 		zle reset-prompt >/dev/null 2>&1 || true
 		[[ -z "$session" ]] && return
 		sesh connect $session
