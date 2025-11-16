362c39c53adf0575b0e915f6c011a321ac91d20f
🤖 automated <cron@job>
2025-11-16 14:45:50 +0545

    Add .config/zsh/README.md Add .config/zsh/completions/fzf.zsh Add .config/zsh/completions/hydectl.zsh Add .config/zsh/conf.d/00-hyde.zsh Add .config/zsh/conf.d/hyde/env.zsh Add .config/zsh/conf.d/hyde/prompt.zsh Add .config/zsh/conf.d/hyde/terminal.zsh Add .config/zsh/config/aliases Add .config/zsh/config/compinit.zsh Add .config/zsh/config/completions.zsh Add .config/zsh/config/insulter.zsh Add .config/zsh/config/keybinds.zsh Add .config/zsh/config/options.zsh Add .config/zsh/.abbreviations Add .config/zsh/.p10k.zsh Add .config/zsh/.zlogin Add .config/zsh/.zlogout Add .config/zsh/.zshenv Add .config/zsh/.zshrc Add .config/zsh/functions/bat.zsh Add .config/zsh/functions/duf.zsh Add .config/zsh/functions/error-handlers.zsh Add .config/zsh/functions/eza.zsh Add .config/zsh/functions/fzf.zsh Add .config/zsh/plugin.zsh Add .config/zsh/prompt.zsh Add .config/zsh/user.zsh Add .config/zsh/zcompletions/_git-extras Add .config/zsh/zcompletions/_just Add .config/zsh/zcompletions/_npm Add .config/zsh/zfunctions/bat.zsh Add .config/zsh/zfunctions/docker.zsh Add .config/zsh/zfunctions/duf.zsh Add .config/zsh/zfunctions/eza.zsh Add .config/zsh/zfunctions/fzf.zsh Add .config/zsh/zfunctions/git.zsh Add .config/zsh/zfunctions/utils.zsh

---
+++ private_dot_config/zsh/config/executable_keybinds.zsh
@@ -0,0 +1,16 @@
+# keybinds
+function sesh-sessions() {
+	{
+		exec </dev/tty
+		exec <&1
+		local session
+		session=$(sesh list -i | fzf --ansi --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+		zle reset-prompt >/dev/null 2>&1 || true
+		[[ -z "$session" ]] && return
+		sesh connect $session
+	}
+}
+
+zle -N sesh-sessions
+bindkey -M vicmd '^O' sesh-sessions
+bindkey -M viins '^O' sesh-sessions
