0cff70c684245d07bbc627c82ee921bfeddf878b
🤖 automated <cron@job>
2025-11-18 12:01:46 +0545

    Add .config/mpv/README.md Add .config/mpv/fonts/fluent-system-icons.ttf Add .config/mpv/fonts/material-design-icons.ttf Add .config/mpv/input.conf Add .config/mpv/mpv.conf Add .config/mpv/profiles.conf Add .config/mpv/script-opts/confirm_resume.conf Add .config/mpv/script-opts/evafast.conf Add .config/mpv/script-opts/modernz.conf Add .config/mpv/script-opts/thumbfast.conf Add .config/mpv/scripts/SmartCopyPaste.lua Add .config/mpv/scripts/autoindent.lua Add .config/mpv/scripts/autoload.lua Add .config/mpv/scripts/confirm_resume.lua Add .config/mpv/scripts/evafast.lua Add .config/mpv/scripts/misc.lua Add .config/mpv/scripts/modernz.lua Add .config/mpv/scripts/sponsorblock_minimal.lua Add .config/mpv/scripts/subit.lua Add .config/mpv/scripts/thumbfast.lua Add .config/mpv/scripts/vlccrop.lua Add .config/mpv/shaders/Anime4K_AutoDownscalePre_x2.glsl Add .config/mpv/shaders/Anime4K_AutoDownscalePre_x4.glsl Add .config/mpv/shaders/Anime4K_Clamp_Highlights.glsl Add .config/mpv/shaders/Anime4K_Darken_Fast.glsl Add .config/mpv/shaders/Anime4K_Darken_HQ.glsl Add .config/mpv/shaders/Anime4K_Darken_VeryFast.glsl Add .config/mpv/shaders/Anime4K_Deblur_DoG.glsl Add .config/mpv/shaders/Anime4K_Deblur_Original.glsl Add .config/mpv/shaders/Anime4K_Denoise_Bilateral_Mean.glsl Add .config/mpv/shaders/Anime4K_Denoise_Bilateral_Median.glsl Add .config/mpv/shaders/Anime4K_Denoise_Bilateral_Mode.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_L.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_M.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_S.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_Soft_L.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_Soft_M.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_Soft_S.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_Soft_UL.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_Soft_VL.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_UL.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_VL.glsl Add .config/mpv/shaders/Anime4K_Thin_Fast.glsl Add .config/mpv/shaders/Anime4K_Thin_HQ.glsl Add .config/mpv/shaders/Anime4K_Thin_VeryFast.glsl Add .config/mpv/shaders/Anime4K_Upscale_CNN_x2_L.glsl Add .config/mpv/shaders/Anime4K_Upscale_CNN_x2_M.glsl Add .config/mpv/shaders/Anime4K_Upscale_CNN_x2_S.glsl Add .config/mpv/shaders/Anime4K_Upscale_CNN_x2_UL.glsl Add .config/mpv/shaders/Anime4K_Upscale_CNN_x2_VL.glsl Add .config/mpv/shaders/Anime4K_Upscale_DTD_x2.glsl Add .config/mpv/shaders/Anime4K_Upscale_Deblur_DoG_x2.glsl Add .config/mpv/shaders/Anime4K_Upscale_Deblur_Original_x2.glsl Add .config/mpv/shaders/Anime4K_Upscale_Denoise_CNN_x2_L.glsl Add .config/mpv/shaders/Anime4K_Upscale_Denoise_CNN_x2_M.glsl Add .config/mpv/shaders/Anime4K_Upscale_Denoise_CNN_x2_S.glsl Add .config/mpv/shaders/Anime4K_Upscale_Denoise_CNN_x2_UL.glsl Add .config/mpv/shaders/Anime4K_Upscale_Denoise_CNN_x2_VL.glsl Add .config/mpv/shaders/Anime4K_Upscale_DoG_x2.glsl Add .config/mpv/shaders/Anime4K_Upscale_Original_x2.glsl Add .config/mpv/shaders/ArtCNN_C4F16.glsl Add .config/mpv/shaders/ArtCNN_C4F16_DN.glsl Add .config/mpv/shaders/ArtCNN_C4F16_DS.glsl Add .config/mpv/shaders/ArtCNN_C4F32.glsl Add .config/mpv/shaders/ArtCNN_C4F32_DN.glsl Add .config/mpv/shaders/ArtCNN_C4F32_DS.glsl

---
+++ private_dot_config/mpv/scripts/confirm_resume.lua
@@ -0,0 +1,193 @@
+--[[
+MPV Confirm Resume Script
+Author: Rubin Bhandari + ChatGPT
+Configurable via script-opts/confirm_resume.conf
+Supports:
+- auto_resume (true/false)
+- save_interval (seconds)
+]]
+
+local utils = require("mp.utils")
+local options = require("mp.options")
+
+
+-- =======================
+-- Default Options
+-- =======================
+local o = {
+  resume_text = "⏪ Resume at %ds? (Y/N) ?",
+  wait_interval = 5,
+  max_entries = 200,
+  auto_resume = false,   -- automatically resume without asking
+  save_interval = 3      -- seconds to debounce saving
+}
+
+-- Load user options
+options.read_options(o)
+
+
+
+-- =======================
+-- Variables
+-- =======================
+-- Use MPV data directory for runtime files
+local data_dir = os.getenv("XDG_DATA_HOME") or os.getenv("HOME") .. "/.local/share"
+data_dir = data_dir .. "/mpv"
+local resume_file = data_dir .. "/resume.json"
+local resume_data = {}
+local save_timer = nil
+local resume_pending = nil
+local resume_dirty = false
+
+----------------------------------------------------------------------
+-- Ensure resume.json exists
+----------------------------------------------------------------------
+local function ensure_resume_file()
+  local dir = resume_file:match("(.+)/[^/]+$")
+  os.execute(string.format('mkdir -p "%s"', dir))
+  local f = io.open(resume_file, "r")
+  if not f then
+    f = io.open(resume_file, "w")
+    if f then
+      f:write("{}")
+      f:close()
+    end
+  else
+    f:close()
+  end
+end
+
+----------------------------------------------------------------------
+-- Load resume data once
+----------------------------------------------------------------------
+local function load_resume()
+  if next(resume_data) ~= nil then return end
+  ensure_resume_file()
+  local f = io.open(resume_file, "r")
+  if not f then return end
+  local text = f:read("*a")
+  f:close()
+  local data = utils.parse_json(text)
+  if type(data) == "table" then
+    resume_data = data
+  end
+end
+
+----------------------------------------------------------------------
+-- Atomic save
+----------------------------------------------------------------------
+local function save_resume()
+  if not resume_dirty then return end
+  ensure_resume_file()
+  local tmp = resume_file .. ".tmp"
+  local f = io.open(tmp, "w")
+  if f then
+    f:write(utils.format_json(resume_data))
+    f:close()
+    os.rename(tmp, resume_file)
+    resume_dirty = false
+  end
+end
+
+----------------------------------------------------------------------
+-- Debounced save (controlled by save_interval)
+----------------------------------------------------------------------
+local function schedule_save()
+  resume_dirty = true
+  if save_timer then
+    save_timer:kill()
+  end
+  save_timer = mp.add_timeout(o.save_interval, function()
+    save_resume()
+    save_timer = nil
+  end)
+end
+
+----------------------------------------------------------------------
+-- Trim resume_data to max_entries
+----------------------------------------------------------------------
+local function trim_resume_data()
+  local count = 0
+  local paths = {}
+  for path in pairs(resume_data) do
+    count = count + 1
+    table.insert(paths, path)
+  end
+  if count <= o.max_entries then return end
+  while count > o.max_entries do
+    local path_to_remove = table.remove(paths, 1)
+    resume_data[path_to_remove] = nil
+    count = count - 1
+  end
+end
+
+----------------------------------------------------------------------
+-- Prompt user to resume
+----------------------------------------------------------------------
+local function prompt_resume(path, pos)
+  if o.auto_resume then
+    mp.set_property_number("time-pos", pos)
+    mp.osd_message(string.format("⏪ Resumed at %ds", math.floor(pos)))
+    return
+  end
+
+  resume_pending = { path = path, pos = pos }
+  mp.osd_message(string.format(o.resume_text, math.floor(pos)), o.wait_interval)
+  mp.add_timeout(o.wait_interval * 60, function()
+    if resume_pending then
+      mp.osd_message("▶ Starting from beginning")
+      resume_pending = nil
+    end
+  end)
+end
+
+----------------------------------------------------------------------
+-- Key bindings
+----------------------------------------------------------------------
+local function do_resume()
+  if resume_pending then
+    mp.set_property_number("time-pos", resume_pending.pos)
+    mp.osd_message(string.format("⏪ Resumed at %ds", math.floor(resume_pending.pos)))
+    resume_pending = nil
+  end
+end
+
+local function skip_resume()
+  if resume_pending then
+    mp.osd_message("▶ Starting from beginning")
+    resume_pending = nil
+  end
+end
+
+mp.add_forced_key_binding("y", "resume-yes", do_resume)
+mp.add_forced_key_binding("Y", "resume-yes-upper", do_resume)
+mp.add_forced_key_binding("n", "resume-no", skip_resume)
+mp.add_forced_key_binding("N", "resume-no-upper", skip_resume)
+
+----------------------------------------------------------------------
+-- File loaded event
+----------------------------------------------------------------------
+mp.register_event("file-loaded", function()
+  load_resume()
+  local path = mp.get_property("path")
+  if path and not path:match("^https?://") and resume_data[path] then
+    prompt_resume(path, resume_data[path])
+  end
+end)
+
+----------------------------------------------------------------------
+-- Track playback position
+----------------------------------------------------------------------
+mp.observe_property("time-pos", "number", function(_, pos)
+  local path = mp.get_property("path")
+  if path and pos and not path:match("^https?://") then
+    resume_data[path] = pos
+    trim_resume_data()
+    schedule_save()
+  end
+end)
+
+----------------------------------------------------------------------
+-- Save on exit
+----------------------------------------------------------------------
+mp.register_event("shutdown", save_resume)
