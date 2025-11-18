d2f97018707666e839065920d54f9ebb1d883c16
🤖 automated <cron@job>
2025-11-18 12:00:14 +0545

    Add .config/mpv/README.md Add .config/mpv/fonts/fluent-system-icons.ttf Add .config/mpv/fonts/material-design-icons.ttf Add .config/mpv/input.conf Add .config/mpv/mpv.conf Add .config/mpv/profiles.conf Add .config/mpv/script-opts/confirm_resume.conf Add .config/mpv/script-opts/evafast.conf Add .config/mpv/script-opts/modernz.conf Add .config/mpv/script-opts/thumbfast.conf Add .config/mpv/scripts/SmartCopyPaste.lua Add .config/mpv/scripts/autoindent.lua Add .config/mpv/scripts/autoload.lua Add .config/mpv/scripts/confirm_resume.lua Add .config/mpv/scripts/evafast.lua Add .config/mpv/scripts/image-bindings.lua Add .config/mpv/scripts/misc.lua Add .config/mpv/scripts/modernz.lua Add .config/mpv/scripts/sponsorblock_minimal.lua Add .config/mpv/scripts/subit.lua Add .config/mpv/scripts/thumbfast.lua Add .config/mpv/scripts/vlccrop.lua Add .config/mpv/shaders/Anime4K_AutoDownscalePre_x2.glsl Add .config/mpv/shaders/Anime4K_AutoDownscalePre_x4.glsl Add .config/mpv/shaders/Anime4K_Clamp_Highlights.glsl Add .config/mpv/shaders/Anime4K_Darken_Fast.glsl Add .config/mpv/shaders/Anime4K_Darken_HQ.glsl Add .config/mpv/shaders/Anime4K_Darken_VeryFast.glsl Add .config/mpv/shaders/Anime4K_Deblur_DoG.glsl Add .config/mpv/shaders/Anime4K_Deblur_Original.glsl Add .config/mpv/shaders/Anime4K_Denoise_Bilateral_Mean.glsl Add .config/mpv/shaders/Anime4K_Denoise_Bilateral_Median.glsl Add .config/mpv/shaders/Anime4K_Denoise_Bilateral_Mode.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_L.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_M.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_S.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_Soft_L.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_Soft_M.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_Soft_S.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_Soft_UL.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_Soft_VL.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_UL.glsl Add .config/mpv/shaders/Anime4K_Restore_CNN_VL.glsl Add .config/mpv/shaders/Anime4K_Thin_Fast.glsl Add .config/mpv/shaders/Anime4K_Thin_HQ.glsl Add .config/mpv/shaders/Anime4K_Thin_VeryFast.glsl Add .config/mpv/shaders/Anime4K_Upscale_CNN_x2_L.glsl Add .config/mpv/shaders/Anime4K_Upscale_CNN_x2_M.glsl Add .config/mpv/shaders/Anime4K_Upscale_CNN_x2_S.glsl Add .config/mpv/shaders/Anime4K_Upscale_CNN_x2_UL.glsl Add .config/mpv/shaders/Anime4K_Upscale_CNN_x2_VL.glsl Add .config/mpv/shaders/Anime4K_Upscale_DTD_x2.glsl Add .config/mpv/shaders/Anime4K_Upscale_Deblur_DoG_x2.glsl Add .config/mpv/shaders/Anime4K_Upscale_Deblur_Original_x2.glsl Add .config/mpv/shaders/Anime4K_Upscale_Denoise_CNN_x2_L.glsl Add .config/mpv/shaders/Anime4K_Upscale_Denoise_CNN_x2_M.glsl Add .config/mpv/shaders/Anime4K_Upscale_Denoise_CNN_x2_S.glsl Add .config/mpv/shaders/Anime4K_Upscale_Denoise_CNN_x2_UL.glsl Add .config/mpv/shaders/Anime4K_Upscale_Denoise_CNN_x2_VL.glsl Add .config/mpv/shaders/Anime4K_Upscale_DoG_x2.glsl Add .config/mpv/shaders/Anime4K_Upscale_Original_x2.glsl Add .config/mpv/shaders/ArtCNN_C4F16.glsl Add .config/mpv/shaders/ArtCNN_C4F16_DN.glsl Add .config/mpv/shaders/ArtCNN_C4F16_DS.glsl Add .config/mpv/shaders/ArtCNN_C4F32.glsl Add .config/mpv/shaders/ArtCNN_C4F32_DN.glsl Add .config/mpv/shaders/ArtCNN_C4F32_DS.glsl

---
+++ private_dot_config/mpv/scripts/misc.lua
@@ -0,0 +1,111 @@
+--When seeking display position and duration like vlc
+mp.register_event("seek", function()
+    local pos = mp.get_property_number("time-pos") or 0
+    local dur = mp.get_property_number("duration") or 0
+
+    local function format_time(t)
+        local h = math.floor(t / 3600)
+        local m = math.floor((t % 3600) / 60)
+        local s = math.floor(t % 60)
+        if h > 0 then
+            return string.format("%d:%02d:%02d", h, m, s)
+        else
+            return string.format("%02d:%02d", m, s)
+        end
+    end
+
+    local msg = format_time(pos) .. " / " .. format_time(dur)
+    mp.osd_message(msg, 2)  -- show for 2 seconds
+end)
+
+
+-- -- 🕓 Auto-skip intros shorter than 60 seconds (if chapters exist)
+-- mp.register_event("file-loaded", function()
+--     local chapters = mp.get_property_native("chapter-list")
+--     if chapters and #chapters > 0 then
+--         local first = chapters[1]
+--         if first.time < 60 then
+--             mp.commandv("seek", first.time + 10, "absolute", "exact")
+--             mp.osd_message("Skipped intro to " .. first.time + 10 .. "s")
+--         end
+--     end
+-- end)
+
+
+-- 🧠 8. Auto-load subtitles in same folder
+
+-- Useful when filenames don’t match exactly.
+
+mp.register_event("file-loaded", function()
+    local path = mp.get_property("path")
+    if not path then return end
+    local dir = string.match(path, "(.+)/[^/]+$")
+    local files = io.popen('ls "' .. dir .. '"'):lines()
+    for f in files do
+        if f:match("%.srt$") or f:match("%.ass$") then
+            mp.commandv("sub-add", dir .. "/" .. f)
+        end
+    end
+end)
+
+
+
+-- This script pauses playback when minimizing the window, and resumes playback
+-- if it's brought back again. If the player was already paused when minimizing,
+-- then try not to mess with the pause state.
+
+local did_minimize = false
+
+mp.observe_property("window-minimized", "bool", function(_, value)
+    local pause = mp.get_property_native("pause")
+    if value == true then
+        if pause == false then
+            mp.set_property_native("pause", true)
+            did_minimize = true
+        end
+    elseif value == false then
+        if did_minimize and (pause == true) then
+            mp.set_property_native("pause", false)
+        end
+        did_minimize = false
+    end
+end)
+
+
+
+-- This snippet creates a simple pause indicator overlay
+local ov = mp.create_osd_overlay('ass-events')
+ov.data = [[{\an5\p1\alpha&H79\1c&Hffffff&\3a&Hff\pos(760,440)}]] ..
+    [[m-125 -75 l 2 2 l -125 75]]
+
+mp.observe_property('pause', 'bool', function(_, paused)
+    mp.add_timeout(0.1, function()
+        if paused then
+            ov:update()
+        else
+            ov:remove()
+        end
+    end)
+end)
+
+
+
+-- This script makes mpv disable ontop when pausing and re-enable it again when resuming playback
+--please note that this won't do anything if ontop was not enabled before pausing
+
+local was_ontop = false
+
+mp.observe_property("pause", "bool", function(_, value)
+    local ontop = mp.get_property_native("ontop")
+    if value then
+        if ontop then
+            mp.set_property_native("ontop", false)
+            was_ontop = true
+        end
+    else
+        if was_ontop and not ontop then
+            mp.set_property_native("ontop", true)
+        end
+        was_ontop = false
+    end
+end)
