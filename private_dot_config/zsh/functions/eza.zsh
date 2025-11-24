48cdda6bf89f2fc2eaf5165a0476955a3142348d
🤖 automated <cron@job>
2025-11-24 23:53:56 +0545

    Remove .config/zsh/README.md Remove .config/zsh/completions/fzf.zsh Remove .config/zsh/completions/hydectl.zsh Remove .config/zsh/conf.d/00-hyde.zsh Remove .config/zsh/conf.d/hyde/env.zsh Remove .config/zsh/conf.d/hyde/prompt.zsh Remove .config/zsh/conf.d/hyde/terminal.zsh Remove .config/zsh/config/aliases Remove .config/zsh/config/compinit.zsh Remove .config/zsh/config/completions.zsh Remove .config/zsh/config/insulter.zsh Remove .config/zsh/config/keybinds.zsh Remove .config/zsh/config/options.zsh Remove .config/zsh/.abbreviations Remove .config/zsh/.p10k.zsh Remove .config/zsh/.zlogin Remove .config/zsh/.zlogout Remove .config/zsh/.zshenv Remove .config/zsh/.zshrc Remove .config/zsh/functions/bat.zsh Remove .config/zsh/functions/duf.zsh Remove .config/zsh/functions/error-handlers.zsh Remove .config/zsh/functions/eza.zsh Remove .config/zsh/functions/fzf.zsh Remove .config/zsh/plugin.zsh Remove .config/zsh/prompt.zsh Remove .config/zsh/user.zsh Remove .config/zsh/zcompletions/_git-extras Remove .config/zsh/zcompletions/_just Remove .config/zsh/zcompletions/_npm Remove .config/zsh/zfunctions/bat.zsh Remove .config/zsh/zfunctions/docker.zsh Remove .config/zsh/zfunctions/duf.zsh Remove .config/zsh/zfunctions/eza.zsh Remove .config/zsh/zfunctions/fzf.zsh Remove .config/zsh/zfunctions/git.zsh Remove .config/zsh/zfunctions/utils.zsh

--- private_dot_config/zsh/functions/eza.zsh
+++
@@ -1,6 +0,0 @@
-if command -v "eza" &>/dev/null; then
-    alias l='eza -lh --icons=auto' \
-        ll='eza -lha --icons=auto --sort=name --group-directories-first' \
-        ld='eza -lhD --icons=auto' \
-        lt='eza --icons=auto --tree'
-fi
