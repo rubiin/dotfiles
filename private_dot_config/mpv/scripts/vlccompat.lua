b77b556ce7fc2352eb4e4e001f3168d9c3bdc336
🤖 automated <cron@job>
2026-03-12 21:36:58 +0545

    Remove .config/mpv/modernz-icons.ttf Add .config/mpv/modernz-icons.ttf Update .config/mpv/script-opts/modernz.conf Add .config/mpv/script-opts/pause_indicator_lite.conf Add .config/mpv/scripts/pause_indicator_lite.lua Update .config/mpv/scripts/vlccompat.lua

--- private_dot_config/mpv/scripts/vlccompat.lua
+++ private_dot_config/mpv/scripts/vlccompat.lua
@@ -23,20 +23,3 @@ mp.register_event("seek", function()
   local msg = format_time(pos) .. " / " .. format_time(dur)
   mp.osd_message(msg, 2)   -- show for 2 seconds
 end)
-
-
-
--- This script creates a simple pause indicator overlay
-local ov = mp.create_osd_overlay('ass-events')
-ov.data = [[{\an5\p1\alpha&H79\1c&Hffffff&\3a&Hff\pos(760,440)}]] ..
-    [[m-125 -75 l 2 2 l -125 75]]
-
-mp.observe_property('pause', 'bool', function(_, paused)
-  mp.add_timeout(0.1, function()
-    if paused then
-      ov:update()
-    else
-      ov:remove()
-    end
-  end)
-end)
