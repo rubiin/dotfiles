cc2ef002a56eec6da035427f745f39077ee0e201
Rubin Bhandari <roobin.bhandari@gmail.com>
2026-03-23 00:58:36 +0545

    Update .config/lite-xl/user_settings.lua

--- private_dot_config/lite-xl/user_settings.lua
+++ private_dot_config/lite-xl/user_settings.lua
@@ -3,13 +3,13 @@ return {
     ["code_font"] = {
       ["fonts"] = {
         [1] = {
-          ["name"] = "MonoLisa Nerd Font Regular",
-          ["path"] = "/home/rubin/.local/share/fonts/MonoLisa/MonoLisaNerdFont-Regular.ttf"
+          ["name"] = "MonoLisa Regular",
+          ["path"] = "/home/rubin/.local/share/fonts/MonoLisa/MonoLisa-Regular.ttf",
         },
         [2] = {
           ["name"] = "JetBrains Mono Regular",
-          ["path"] = "/usr/share/lite-xl/fonts/JetBrainsMono-Regular.ttf"
-        }
+          ["path"] = "/usr/share/lite-xl/fonts/JetBrainsMono-Regular.ttf",
+        },
       },
       ["options"] = {
         ["antialiasing"] = "subpixel",
@@ -19,20 +19,20 @@ return {
         ["size"] = 15,
         ["smoothing"] = false,
         ["strikethrough"] = false,
-        ["underline"] = false
-      }
+        ["underline"] = false,
+      },
     },
     ["disable_blink"] = false,
     ["font"] = {
       ["fonts"] = {
         [1] = {
           ["name"] = "MonoLisa Nerd Font Regular",
-          ["path"] = "/home/rubin/.local/share/fonts/MonoLisa/MonoLisaNerdFont-Regular.ttf"
+          ["path"] = "/home/rubin/.local/share/fonts/MonoLisa/MonoLisaNerdFont-Regular.ttf",
         },
         [2] = {
           ["name"] = "Fira Sans Regular",
-          ["path"] = "/usr/share/lite-xl/fonts/FiraSans-Regular.ttf"
-        }
+          ["path"] = "/usr/share/lite-xl/fonts/FiraSans-Regular.ttf",
+        },
       },
       ["options"] = {
         ["antialiasing"] = "subpixel",
@@ -42,8 +42,8 @@ return {
         ["size"] = 14,
         ["smoothing"] = false,
         ["strikethrough"] = false,
-        ["underline"] = false
-      }
+        ["underline"] = false,
+      },
     },
     ["plugins"] = {
       ["code_plus"] = {
@@ -51,58 +51,58 @@ return {
           [1] = 239,
           [2] = 99,
           [3] = 133,
-          [4] = 255
+          [4] = 255,
         },
         ["todo"] = {
           [1] = 85,
           [2] = 146,
           [3] = 207,
-          [4] = 255
-        }
+          [4] = 255,
+        },
       },
       ["gitblame"] = {
         ["text_color"] = {
           [1] = 183,
           [2] = 183,
           [3] = 183,
-          [4] = 255
-        }
+          [4] = 255,
+        },
       },
       ["minimap"] = {
         ["caret_color"] = {
           [1] = 147,
           [2] = 221,
           [3] = 250,
-          [4] = 255
+          [4] = 255,
         },
         ["selection_color"] = {
           [1] = 82,
           [2] = 82,
           [3] = 87,
-          [4] = 255
-        }
+          [4] = 255,
+        },
       },
       ["terminal"] = {
         ["background"] = {
           [1] = 46,
           [2] = 46,
           [3] = 50,
-          [4] = 255
+          [4] = 255,
         },
         ["text"] = {
           [1] = 225,
           [2] = 225,
           [3] = 230,
-          [4] = 255
-        }
+          [4] = 255,
+        },
       },
       ["todotreeview"] = {
         ["todo_mode"] = "tag",
-        ["todo_scope"] = "focused"
-      }
+        ["todo_scope"] = "focused",
+      },
     },
     ["statusbar"] = {
-      ["enabled"] = true
-    }
-  }
+      ["enabled"] = true,
+    },
+  },
 }
