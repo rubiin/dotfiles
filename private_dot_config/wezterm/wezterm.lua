8f3750c1c34a3a94d8de5ab09cbcc06a04e1a2b1
Rubin Bhandari <roobin.bhandari@gmail.com>
2026-03-23 00:55:50 +0545

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -32,7 +32,7 @@ config.hyperlink_rules = wezterm.default_hyperlink_rules()
 config.line_height = 1.1
 config.font_size = 10
 config.font = wezterm.font_with_fallback({
-  { family = "MonoLisa Nerd Font", weight = "Regular" },
+  { family = "MonoLisa Regular", weight = "Regular" },
   { family = "DejaVuSansM Nerd Font", weight = "Regular" },
   "Noto Color Emoji",
   { family = "Symbols Nerd Font Mono", scale = 0.75 },
