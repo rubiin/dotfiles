dfc0e3f1d841f49658073f5ca3fc85ab52a58ec7
Rubin Bhandari <roobin.bhandari@gmail.com>
2026-03-25 00:29:01 +0545

    Update chores.sh

--- run_onchange_chores.sh
+++ run_onchange_chores.sh
@@ -3,7 +3,7 @@
 # use this script to perform cleaaning or anything after applying updates
 
 mise install
-mise prune
+mise prune -y
 bat cache --build
 hydectl reload &
 notify-send "Chezmoi finished updating dotfiles"
