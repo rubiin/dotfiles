2ddae2edfe9b50e71fa0bbc09d9621ec5911ab18
Rubin Bhandari <roobin.bhandari@gmail.com>
2026-03-25 19:58:26 +0545

    Remove .config/zsh/config/aliases Remove .config/zsh/config/compinit.zsh Remove .config/zsh/config/completions.zsh Remove .config/zsh/config/insulter.zsh Remove .config/zsh/config/keybinds.zsh Remove .config/zsh/config/options.zsh Remove .config/zsh/.p10k.zsh Remove .config/zsh/.zlogin Remove .config/zsh/.zshenv Remove .config/zsh/.zshrc Remove .config/zsh/zcompletions/_bat Remove .config/zsh/zcompletions/_docker Remove .config/zsh/zcompletions/_gh Remove .config/zsh/zcompletions/_npm Remove .config/zsh/zcompletions/_just Remove .config/zsh/zfunctions/bat.zsh Remove .config/zsh/zfunctions/docker.zsh Remove .config/zsh/zfunctions/duf.zsh Remove .config/zsh/zfunctions/eza.zsh Remove .config/zsh/zfunctions/fzf.zsh Remove .config/zsh/zfunctions/git.zsh Remove .config/zsh/zfunctions/utils.zsh Update chores.sh

--- run_onchange_chores.sh
+++ run_onchange_chores.sh
@@ -14,7 +14,7 @@ ZSH_COMPLETIONS_DIR="$CONFIG_DIR/zsh/zcompletions"
 
 echo "Generating Zsh completions in $ZSH_COMPLETIONS_DIR..."
 npm completion >"$ZSH_COMPLETIONS_DIR/_npm"
-just --completion zsh >"$ZSH_COMPLETIONS_DIR/_just"
+just --completions zsh >"$ZSH_COMPLETIONS_DIR/_just"
 gh completion -s zsh >"$ZSH_COMPLETIONS_DIR/_gh"
 docker completion zsh >"$ZSH_COMPLETIONS_DIR/_docker"
 
