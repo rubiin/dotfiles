2df5cf685bf3a3660c351dc4610e9eea790a7767
Rubin Bhandari <roobin.bhandari@gmail.com>
2026-03-25 19:40:22 +0545

    Update chores.sh

--- run_onchange_chores.sh
+++ run_onchange_chores.sh
@@ -10,9 +10,14 @@ hydectl reload &
 # Set your completions folder
 CONFIG_DIR="${HOME}/.config"
 ZSH_COMPLETIONS_DIR="$CONFIG_DIR/zsh/zcompletions"
+
+
 echo "Generating Zsh completions in $ZSH_COMPLETIONS_DIR..."
 npm completion >"$ZSH_COMPLETIONS_DIR/_npm"
 just --zsh-completion >"$ZSH_COMPLETIONS_DIR/_just"
 gh completion -s zsh >"$ZSH_COMPLETIONS_DIR/_gh"
+docker completion zsh >"$ZSH_COMPLETIONS_DIR/_docker"
+docker completion zsh >"$ZSH_COMPLETIONS_DIR/_docker"
+bat --completion zsh > "$ZSH_COMPLETIONS_DIR/_bat"
 
 notify-send "Chezmoi finished updating dotfiles"
