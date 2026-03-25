dcc597f4529c64ef1f0450d7fcbd0459ccf7d5f1
Rubin Bhandari <roobin.bhandari@gmail.com>
2026-03-25 19:56:55 +0545

    Update .chezmoiexternal.toml Update chores.sh

--- run_onchange_chores.sh
+++ run_onchange_chores.sh
@@ -14,9 +14,9 @@ ZSH_COMPLETIONS_DIR="$CONFIG_DIR/zsh/zcompletions"
 
 echo "Generating Zsh completions in $ZSH_COMPLETIONS_DIR..."
 npm completion >"$ZSH_COMPLETIONS_DIR/_npm"
-just --zsh-completion >"$ZSH_COMPLETIONS_DIR/_just"
+just --completion zsh >"$ZSH_COMPLETIONS_DIR/_just"
 gh completion -s zsh >"$ZSH_COMPLETIONS_DIR/_gh"
 docker completion zsh >"$ZSH_COMPLETIONS_DIR/_docker"
-bat --completion zsh > "$ZSH_COMPLETIONS_DIR/_bat"
+
 
 notify-send "Chezmoi finished updating dotfiles"
