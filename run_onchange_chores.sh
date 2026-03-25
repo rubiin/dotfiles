ab0befd81054024928006eb708d7bc8c717d4347
Rubin Bhandari <roobin.bhandari@gmail.com>
2026-03-25 22:29:00 +0545

    Update chores.sh

--- run_onchange_chores.sh
+++ run_onchange_chores.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-# use this script to perform cleaaning or anything after applying updates
+# use this script to perform cleaning or anything after applying updates
 
-mise install
-mise prune -y
+mise install && mise prune -y
 bat cache --build
-hydectl reload &
 
 # Set your completions folder
 CONFIG_DIR="${HOME}/.config"
@@ -17,6 +15,8 @@ npm completion >"$ZSH_COMPLETIONS_DIR/_npm"
 just --completions zsh >"$ZSH_COMPLETIONS_DIR/_just"
 gh completion -s zsh >"$ZSH_COMPLETIONS_DIR/_gh"
 docker completion zsh >"$ZSH_COMPLETIONS_DIR/_docker"
+mise completion zsh >"$ZSH_COMPLETIONS_DIR/_mise"
 
+hydectl reload &
 
 notify-send "Chezmoi finished updating dotfiles"
